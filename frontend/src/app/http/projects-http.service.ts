d41d31339ee4d4753e16af39118104ad0cc9c803
Jahmal Blanchard <45908841+jablancTheDesigner@users.noreply.github.com>
2022-01-30 00:35:59 -0500

    update project details layout

--- frontend/src/app/http/projects-http.service.ts
+++ frontend/src/app/http/projects-http.service.ts
@@ -11,25 +11,64 @@ export class ProjectsHttpService {
       id: 1,
       title: 'SWTGLAW.com',
       client: 'Smith & Williams Trial Group',
-      logo: '',
+      thumbnail: {
+        src: 'https://via.placeholder.com/750x469'
+      },
+      gallery: [
+        {
+          src: 'https://via.placeholder.com/750x469'
+        },
+        {
+          src: 'https://via.placeholder.com/750x469'
+        },
+        {
+          src: 'https://via.placeholder.com/750x469'
+        }
+      ],
       url: { link: '', name: '' },
       tools: [],
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pulvinar sapien et ligula ullamcorper malesuada proin libero nunc consequat. Aliquet eget sit amet tellus cras. Lacus sed viverra tellus in hac habitasse platea dictumst vestibulum. Nunc scelerisque viverra mauris in aliquam sem fringilla. Scelerisque fermentum dui faucibus in ornare quam viverra orci. Hac habitasse platea dictumst quisque sagittis purus sit amet volutpat. Habitant morbi tristique senectus et netus et malesuada fames. Mi quis hendrerit dolor magna eget.'
     },
     {
       id: 2,
-      title: 'Puffessional.com',
+      title: 'Puffessional',
       client: 'Puffessional',
-      logo: '',
-      url: { link: '', name: '' },
+      logo: {
+        src: 'https://via.placeholder.com/350x150'
+      },
+      thumbnail: {
+        src: 'https://via.placeholder.com/750x469'
+      },
+      gallery: [
+        {
+          src: 'https://via.placeholder.com/750x469'
+        },
+        {
+          src: 'https://via.placeholder.com/750x469'
+        },
+        {
+          src: 'https://via.placeholder.com/750x469'
+        }
+      ],
+      url: { link: 'https://jablancthedesigner.github.io/puffessional/', name: 'Puffessional.com' },
       tools: [],
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pulvinar sapien et ligula ullamcorper malesuada proin libero nunc consequat. Aliquet eget sit amet tellus cras. Lacus sed viverra tellus in hac habitasse platea dictumst vestibulum. Nunc scelerisque viverra mauris in aliquam sem fringilla. Scelerisque fermentum dui faucibus in ornare quam viverra orci. Hac habitasse platea dictumst quisque sagittis purus sit amet volutpat. Habitant morbi tristique senectus et netus et malesuada fames. Mi quis hendrerit dolor magna eget.'
     },
     {
       id: 3,
-      title: 'Countdown Me Down App',
+      title: ' Countdown Timer App',
       client: 'Freelance',
-      logo: '',
+      thumbnail: {
+        src: 'https://via.placeholder.com/750x469'
+      },
+      gallery: [
+        {
+          src: 'https://via.placeholder.com/750x469'
+        },
+        {
+          src: 'https://via.placeholder.com/750x469'
+        },
+      ],
       url: { link: 'https://jablancthedesigner.github.io/countdown-timer/', name: 'Countdown Timer' },
       tools: [],
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pulvinar sapien et ligula ullamcorper malesuada proin libero nunc consequat. Aliquet eget sit amet tellus cras. Lacus sed viverra tellus in hac habitasse platea dictumst vestibulum. Nunc scelerisque viverra mauris in aliquam sem fringilla. Scelerisque fermentum dui faucibus in ornare quam viverra orci. Hac habitasse platea dictumst quisque sagittis purus sit amet volutpat. Habitant morbi tristique senectus et netus et malesuada fames. Mi quis hendrerit dolor magna eget.'
