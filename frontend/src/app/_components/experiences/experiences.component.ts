7ebdbd2333f55258dac7f6eb4ea3343316e69fbb
Jahmal Blanchard <45908841+jablancTheDesigner@users.noreply.github.com>
2022-01-30 01:51:11 -0500

    fix experiences section

--- frontend/src/app/_components/experiences/experiences.component.ts
+++ frontend/src/app/_components/experiences/experiences.component.ts
@@ -7,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperiencesComponent implements OnInit {
 
+  experiences: any[] = [
+    {
+      position: 'Software Engineer III',
+      company: 'Westgate Resorts',
+      dateStart: '2021',
+      dateEnd: null,
+      recent: true
+    },
+    {
+      position: 'Software Engineer II',
+      company: 'Westgate Resorts',
+      dateStart: '2019',
+      dateEnd: '2021',
+    },
+    {
+      position: 'Creative Director / Frontend Developer',
+      company: 'Global Tech Network',
+      dateStart: '2017',
+      dateEnd: '2019',
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
