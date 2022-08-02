79157aca9f657e607d6de0b0b7f3f0364880bbc4
Jahmal Blanchard <jablancdesigns@gmail.com>
2022-08-02 17:46:40 -0400

    logo update and sizing

--- frontend/src/app/animations/route-transition-animations.ts
+++ frontend/src/app/animations/route-transition-animations.ts
@@ -1,23 +1,16 @@
 import { animate, animateChild, group, query, style, transition, trigger } from "@angular/animations";
 
-export const routeTransitionAnimations = trigger('triggerName', [
-    transition('* <=> *', [
-        style({ position: 'relative'}),
-        query(':enter, :leave', [
-          style({
-            position: 'absolute',
-            top: 0,
-            right: 0,
-            width: '100%',
-            height: '100%' 
-          })
+export const routeTransitionAnimations = trigger('routerTransition', [
+    transition('* <=> *', [    
+      query(':enter, :leave', style({ position: 'fixed', width:'100%' })),
+      group([ 
+        query(':enter', [
+          style({ transform: 'translateX(100%)' }),
+          animate('0.5s ease-in-out', style({ transform: 'translateX(0%)' }))
         ]),
-        query(':enter', [style({ right: '-100%', opacity: 0 })]),
-        query(':leave', animateChild()),
-        group([
-            query(':leave', [animate('0.5s ease-out', style({ right: '100%', opacity: 0 }))]),
-            query(':enter', [animate('0.5s ease-out', style({ right: '0%', opacity: 1 }))])
-        ]),
-        query(':enter', animateChild())
+        query(':leave', [
+          style({ transform: 'translateX(0%)' }),
+          animate('0.5s ease-in-out', style({ transform: 'translateX(-100%)' }))]),
       ])
-]);
+    ])
+  ]);
